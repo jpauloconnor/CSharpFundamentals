352fd4a2effb59ba70bf135203f52ba463245593
Kenn Pascascio <kenn370@gmail.com>
2017-03-22 10:13:17 -0400

    Woring in the console intro

---
+++ 0.01_Intro/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _0._01_Intro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World");
+            Console.ReadLine();
+
+        }
+    }
+}
